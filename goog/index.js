a12928c7a8955537e718efa301e75f82f6039ade
mat <73811184+M5X11@users.noreply.github.com>
2025-02-01 03:37:00 +0100

    trinkets and other

--- goog/index.js
+++ goog/index.js
@@ -15,182 +15,182 @@ init();
 animate();
 
 function init() {
-    googtainer = document.getElementById("googtainer");
-    
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.toneMapping = THREE.AgXToneMapping;
-    renderer.toneMappingExposure = 2;
-    renderer.setClearColor(0xffffff, 0);
+	googtainer = document.getElementById("googtainer");
+	
+	scene = new THREE.Scene();
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.toneMapping = THREE.AgXToneMapping;
+	renderer.toneMappingExposure = 2;
+	renderer.setClearColor(0xffffff, 0);
 
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize( window.innerWidth, window.innerHeight);
 	googtainer.appendChild(renderer.domElement);
 
-    const loader = new GLTFLoader();
-    const dracoLoader = new DRACOLoader();
+	const loader = new GLTFLoader();
+	const dracoLoader = new DRACOLoader();
 
-    dracoLoader.setDecoderConfig({ type: 'js' });
-    dracoLoader.setDecoderPath('//soggy.cat/static/threejs/draco/');
+	dracoLoader.setDecoderConfig({ type: 'js' });
+	dracoLoader.setDecoderPath('//soggy.cat/static/threejs/draco/');
 
-    loader.setDRACOLoader(dracoLoader);
+	loader.setDRACOLoader(dracoLoader);
 
-    texloader = new THREE.TextureLoader();
+	texloader = new THREE.TextureLoader();
 
-    const frameTex = texloader.load('scene/frameBake.webp'); 
-    frameTex.colorSpace = THREE.SRGBColorSpace;
-    frameTex.flipY = false;
-    frameTex.generateMipmaps = false;
+	const frameTex = texloader.load('scene/frameBake.webp'); 
+	frameTex.colorSpace = THREE.SRGBColorSpace;
+	frameTex.flipY = false;
+	frameTex.generateMipmaps = false;
 
-    const defaultAlbedoTex = texloader.load('img/goog.webp');
-    diffuseTex = texloader.load('scene/googBake.webp');
-    glossyTex = texloader.load('scene/googBakeGlossy.webp');
+	const defaultAlbedoTex = texloader.load('img/goog.webp');
+	diffuseTex = texloader.load('scene/googBake.webp');
+	glossyTex = texloader.load('scene/googBakeGlossy.webp');
 
-    defaultAlbedoTex.flipY = false;
-    defaultAlbedoTex.generateMipmaps = false;
-    diffuseTex.flipY = false;
-    diffuseTex.generateMipmaps = false;
-    glossyTex.flipY = false;
-    glossyTex.generateMipmaps = false;
+	defaultAlbedoTex.flipY = false;
+	defaultAlbedoTex.generateMipmaps = false;
+	diffuseTex.flipY = false;
+	diffuseTex.generateMipmaps = false;
+	glossyTex.flipY = false;
+	glossyTex.generateMipmaps = false;
 
-    const frameMaterial = new THREE.MeshBasicMaterial({
-        map: frameTex
-    });
+	const frameMaterial = new THREE.MeshBasicMaterial({
+		map: frameTex
+	});
 
-    googLightmapMaterial = new THREE.ShaderMaterial({
-        uniforms: {
-            albedo: { value: defaultAlbedoTex },
-            diffuse: { value: diffuseTex },
-            glossy: { value: glossyTex }
-        },
+	googLightmapMaterial = new THREE.ShaderMaterial({
+		uniforms: {
+			albedo: { value: defaultAlbedoTex },
+			diffuse: { value: diffuseTex },
+			glossy: { value: glossyTex }
+		},
 
-        vertexShader: `
-            varying vec2 vUv;
-            void main() {
-                vUv = vec2(uv.x, uv.y);
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-            }
-        `,
+		vertexShader: `
+			varying vec2 vUv;
+			void main() {
+				vUv = vec2(uv.x, uv.y);
+				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+			}
+		`,
 
-        fragmentShader: `
-            varying vec2 vUv;
-            uniform sampler2D albedo;
-            uniform sampler2D diffuse;
-            uniform sampler2D glossy;
-    
-            void main() {
-                vec4 albedoRGBA = texture2D(albedo, vUv);
-                vec3 diffuseColor = texture2D(diffuse, vUv).rgb;
-                vec3 glossyColor = texture2D(glossy, vUv).rgb;
+		fragmentShader: `
+			varying vec2 vUv;
+			uniform sampler2D albedo;
+			uniform sampler2D diffuse;
+			uniform sampler2D glossy;
+	
+			void main() {
+				vec4 albedoRGBA = texture2D(albedo, vUv);
+				vec3 diffuseColor = texture2D(diffuse, vUv).rgb;
+				vec3 glossyColor = texture2D(glossy, vUv).rgb;
 
-                // not sure whether this should be grayscale or not, i think this should be an option
-                // later on, in the side menu or somethin
-                float gray = 0.21 * albedoRGBA.r + 0.71 * albedoRGBA.g + 0.07 * albedoRGBA.b;
-                vec3 composite = (diffuseColor * albedoRGBA.rgb) + glossyColor;
+				// not sure whether this should be grayscale or not, i think this should be an option
+				// later on, in the side menu or somethin
+				float gray = 0.21 * albedoRGBA.r + 0.71 * albedoRGBA.g + 0.07 * albedoRGBA.b;
+				vec3 composite = (diffuseColor * albedoRGBA.rgb) + glossyColor;
 
-                gl_FragColor = vec4(composite * albedoRGBA.a, albedoRGBA.a);
-            }
-        `
-    });
+				gl_FragColor = vec4(composite * albedoRGBA.a, albedoRGBA.a);
+			}
+		`
+	});
 
-    loader.load('scene/scene.glb', (gltf) => {
-        scene.add(gltf.scene);
-        
-        frame = gltf.scene.getObjectByName("Frame");
-        frame.material = frameMaterial;
+	loader.load('scene/scene.glb', (gltf) => {
+		scene.add(gltf.scene);
+		
+		frame = gltf.scene.getObjectByName("Frame");
+		frame.material = frameMaterial;
 
-        goog = gltf.scene.getObjectByName("Goog");
-        goog.material = googLightmapMaterial;
-    });
+		goog = gltf.scene.getObjectByName("Goog");
+		goog.material = googLightmapMaterial;
+	});
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1.2, 4);
-    camera.position.set(0, 0, 2.5);
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1.2, 4);
+	camera.position.set(0, 0, 2.5);
 
-    // i don't know if its even worth using orbit controls at this point :sob:
+	// i don't know if its even worth using orbit controls at this point :sob:
 
-    camStuff();
+	camStuff();
 
-    raycaster = new THREE.Raycaster();
+	raycaster = new THREE.Raycaster();
 
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-    document.addEventListener('mouseleave', mouseLeave);
-    document.addEventListener('soggyupdate', onSoggyUpdate);
+	document.addEventListener('mousemove', onDocumentMouseMove, false);
+	document.addEventListener('mouseleave', mouseLeave);
+	document.addEventListener('soggyupdate', onSoggyUpdate);
 
-    window.addEventListener('resize', onWindowResize, false);
+	window.addEventListener('resize', onWindowResize, false);
 }
 
 function onSoggyUpdate(e) {
-    const newTexture = texloader.load(e.detail.result);
-    newTexture.flipY = false;
-    newTexture.generateMipmaps = false;
+	const newTexture = texloader.load(e.detail.result);
+	newTexture.flipY = false;
+	newTexture.generateMipmaps = false;
 
-    googLightmapMaterial.uniforms.albedo = { value: newTexture };
+	googLightmapMaterial.uniforms.albedo = { value: newTexture };
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function mouseLeave() {
-    updateMouseMovement(-1, -1, true);
+	updateMouseMovement(-1, -1, true);
 }
 
 function onDocumentMouseMove(event) {
-    if (!goog || !frame) return;
-    updateMouseMovement(event.clientX, event.clientY, false);
+	if (!goog || !frame) return;
+	updateMouseMovement(event.clientX, event.clientY, false);
 }
 
 function updateMouseMovement(clientX, clientY, forceUnhover) {
-    let hovering = false;
-    const whacky = 0.3;
+	let hovering = false;
+	const whacky = 0.3;
 
-    const mouse = new THREE.Vector2(
-        (clientX / window.innerWidth) * 2 - 1,
-        -(clientY / window.innerHeight) * 2 + 1
-    );
+	const mouse = new THREE.Vector2(
+		(clientX / window.innerWidth) * 2 - 1,
+		-(clientY / window.innerHeight) * 2 + 1
+	);
 
-    if (!forceUnhover) {
-        raycaster.setFromCamera(mouse, camera);
-        
-        const intersects = raycaster.intersectObjects([frame, goog]);
-        if (intersects.length > 0 && intersects[0].object === goog)
-            hovering = true;
-    }
+	if (!forceUnhover) {
+		raycaster.setFromCamera(mouse, camera);
+		
+		const intersects = raycaster.intersectObjects([frame, goog]);
+		if (intersects.length > 0 && intersects[0].object === goog)
+			hovering = true;
+	}
 
-    if (hovering) {
-        targetFOV = 70;
+	if (hovering) {
+		targetFOV = 70;
 
-        targetRotationX = (-mouse.y * whacky);
-        targetRotationY = (mouse.x * whacky * (window.innerWidth / window.innerHeight));
-    }
-    else {
-        targetFOV = 75;
+		targetRotationX = (-mouse.y * whacky);
+		targetRotationY = (mouse.x * whacky * (window.innerWidth / window.innerHeight));
+	}
+	else {
+		targetFOV = 75;
 
-        targetRotationX = 0;
-        targetRotationY = 0;
-    }
+		targetRotationX = 0;
+		targetRotationY = 0;
+	}
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-    camStuff();
-    camera.updateProjectionMatrix();
-    renderer.render(scene, camera);
+	camStuff();
+	camera.updateProjectionMatrix();
+	renderer.render(scene, camera);
 }
 
 function camStuff() {
-    const lerpedY = THREE.MathUtils.lerp(lastRotationY, targetRotationY, 0.1);
-    const lerpedX = THREE.MathUtils.lerp(lastRotationX, targetRotationX, 0.1);
+	const lerpedY = THREE.MathUtils.lerp(lastRotationY, targetRotationY, 0.1);
+	const lerpedX = THREE.MathUtils.lerp(lastRotationX, targetRotationX, 0.1);
 
-    const lerpedFOV = THREE.MathUtils.lerp(lastFOV, targetFOV, 0.1);
+	const lerpedFOV = THREE.MathUtils.lerp(lastFOV, targetFOV, 0.1);
 
-    lastRotationX = lerpedX;
-    lastRotationY = lerpedY;
+	lastRotationX = lerpedX;
+	lastRotationY = lerpedY;
 
-    if (frame) frame.setRotationFromEuler(new THREE.Euler(-lerpedX - Math.PI / 2, Math.PI/1, lerpedY - Math.PI / 1, 'XYZ'));
-    lastFOV = lerpedFOV;
+	if (frame) frame.setRotationFromEuler(new THREE.Euler(-lerpedX - Math.PI / 2, Math.PI/1, lerpedY - Math.PI / 1, 'XYZ'));
+	lastFOV = lerpedFOV;
 
-    camera.fov = lerpedFOV;
+	camera.fov = lerpedFOV;
 }
--- team/index.js
+++ team/index.js
@@ -3,10 +3,10 @@ let index = 0;
 var MusicLengthInSeconds = 61.9382086168; // length of the music goes here
 var images = [];
 function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
+	for (var i = 0; i < arguments.length; i++) {
+		images[i] = new Image();
+		images[i].src = preload.arguments[i];
+	}
 }
 
 preload("assets/images/boom.awebp");
@@ -22,78 +22,80 @@ player_a.src = "assets/audio/cats.mp3";
 player_b.src = player_a.src;
 
 function playMusic(){
-    var player = null;
+	var player = null;
 
-    if(current_player == "a"){
-        player = player_b;
-        current_player = "b";
-    }
-    else{
-        player = player_a;
-        current_player = "a";
-    }
+	if(current_player == "a"){
+		player = player_b;
+		current_player = "b";
+	}
+	else{
+		player = player_a;
+		current_player = "a";
+	}
 
-    player.play();
+	player.play();
 
-    setTimeout(playMusic, MusicLengthInSeconds*1000);
+	setTimeout(playMusic, MusicLengthInSeconds*1000);
 }
 
 
 function showContainer(){
-    const overlay = document.querySelector(".overlay");
-    overlay.style.opacity = "0";
-    overlay.style.pointerEvents = "none";
-    explode.play();
-    animateTitle();
-    playMusic();
-    displayImages();
-  
-    let explosion = document.getElementById('explode');
-    explosion.style.display = 'block';
-    setTimeout(function(){explosion.style.display = 'none';}, 750);
-    // setTimeout(function(){document.write()}, 62000)
+	const overlay = document.querySelector(".overlay");
+	overlay.style.opacity = "0";
+	overlay.style.pointerEvents = "none";
+	explode.play();
+	animateTitle();
+	playMusic();
+	displayImages();
+
+	const explosion = document.getElementById('explode');
+	explosion.style.display = 'block';
+	setTimeout(function(){explosion.style.display = 'none';}, 750);
+	// setTimeout(function(){document.write()}, 62000)
 }
+
+const message = [
+	"|ssoggycat enterprise ",
+	"|soggycat enterprise s",
+	"|oggycat enterprise ss",
+	"|ggycat enterprise sso",
+	"|gycat enterprise ssog",
+	"|ycat enterprise ssogg",
+	"|cat enterprise ssoggy",
+	"|at enterprise ssoggyc",
+	"|t enterprise ssoggyca",
+	"| enterprise ssoggycat",
+	"|enterprise ssoggycat",
+	"|nterprise ssoggycat e",
+	"|terprise ssoggycat en",
+	"|erprise ssoggycat ent",
+	"|rprise ssoggycat ente",
+	"|prise ssoggycat enter",
+	"|rise ssoggycat enterp",
+	"|ise ssoggycat enterpr",
+	"|se ssoggycat enterpri",
+	"|e ssoggycat enterpris",
+	"| ssoggycat enterpris",
+];
+let i = 0;
 
 function animateTitle() {
-      i >= message.length - 1 ? (i = 0) : i++,
-        (document.title = message[i]),
-        setTimeout("animateTitle()", 483.8709676);
+	i >= message.length - 1 ? (i = 0) : i++,
+
+	(document.title = message[i]),
+	setTimeout("animateTitle()", 483.8709676);
 }
 
-var message = [
-  "|ssoggycat enterprise ",
-  "|soggycat enterprise s",
-  "|oggycat enterprise ss",
-  "|ggycat enterprise sso",
-  "|gycat enterprise ssog",
-  "|ycat enterprise ssogg",
-  "|cat enterprise ssoggy",
-  "|at enterprise ssoggyc",
-  "|t enterprise ssoggyca",
-  "| enterprise ssoggycat",
-  "|enterprise ssoggycat",
-  "|nterprise ssoggycat e",
-  "|terprise ssoggycat en",
-  "|erprise ssoggycat ent",
-  "|rprise ssoggycat ente",
-  "|prise ssoggycat enter",
-  "|rise ssoggycat enterp",
-  "|ise ssoggycat enterpr",
-  "|se ssoggycat enterpri",
-  "|e ssoggycat enterpris",
-  "| ssoggycat enterpris",
-], i = 0;
-
 function displayImages() {
-  let i;
-  const images = document.getElementsByClassName("image");
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
-  }
-  index++;
-  if (index > images.length) {
-    index = 1;
-  }
-  images[index-1].style.display = "block";
-  setTimeout(displayImages, 1935.48387); 
+	let i;
+	const images = document.getElementsByClassName("image");
+	for (i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
+	}
+	index++;
+	if (index > images.length) {
+		index = 1;
+	}
+	images[index-1].style.display = "block";
+	setTimeout(displayImages, 1935.48387); 
 }
